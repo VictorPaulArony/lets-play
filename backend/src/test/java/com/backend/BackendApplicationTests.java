faffe55b508b5d0cf6affc18334e233aa36aedf8
victorpaularony <smallypauls@gmail.com>
2026-01-30 14:19:22 +0300

    Remove SpringBootTest annotation from BackendApplicationTests

--- backend/src/test/java/com/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/backend/BackendApplicationTests.java
@@ -1,9 +1,9 @@
 package com.backend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class BackendApplicationTests {
 
 	@Test
