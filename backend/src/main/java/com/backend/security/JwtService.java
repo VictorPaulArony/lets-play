00d310e92f3a3cdbe0be72e1878a4888ddf5070d
victorpaularony <smallypauls@gmail.com>
2026-03-02 08:21:59 +0300

    feat: comment out the secret key configuration in JwtService

--- backend/src/main/java/com/backend/security/JwtService.java
+++ backend/src/main/java/com/backend/security/JwtService.java
@@ -18,7 +18,7 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    @Value("${application.security.jwt.secret-key:}")
+    // @Value("${application.security.jwt.secret-key:}")
     private String secretKey;
 
     @Value("${application.security.jwt.expiration:86400000}")
